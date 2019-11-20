2ba66efc60311c82765c28920316977a5b68d5d5
Luis López <ldotlopez@gmail.com>
2019-11-20 20:44:04 +0100

    Prototyping database

--- arroyo/database.py
+++ arroyo/database.py
@@ -1,195 +1,45 @@
-import abc
-import collections
-import contextlib
-import threading
-import typing
-import pathlib
-import json
-import os
-
-# import tinydb
-# import tinydb.storages
+from arroyo.kit import nodb
 
 
-class Storage:
-    def __init__(self, **kwargs: typing.Dict[str, typing.Any]):
-        pass
-
-    @abc.abstractmethod
-    def read(self) -> typing.Dict[str, typing.Any]:
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def write(self, data: collections.Mapping) -> None:
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def close(self) -> None:
-        raise NotImplementedError()
+class Database(nodb.Database):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.states = self.create_table('states', States)
+        self.links = self.create_table('links', Links)
+        self.id_mapping = self.create_table('id_mapping', IdMapping)
 
 
-class MemoryStorage(Storage):
-    def __init__(self):
-        self.memory = {}
+class IdMapping(nodb.Table):
+    DEFAULTS = {
+        'native': {},
+        'reverse': {}
+    }
 
-    def read(self):
-        return self.memory
+    def map(self, native_id, external_id):
+        self.data['native'][native_id] = external_id
+        self.data['reverse'][external_id] = native_id
 
-    def write(self, data):
-        self.memory = data
+    def get_native(self, external_id):
+        return self.data['reverse'][external_id]
 
-    def close(self):
-        pass
-
-
-class JSONStorage(Storage):
-    def __init__(self, path: pathlib.Path):
-        super().__init__()
-        self.touch(path, create_dirs=True)  # Create file if not exists
-        self._fh = open(path, 'r+', encoding='utf-8')
-
-    def read(self):
-        self._fh.seek(0, os.SEEK_END)
-        size = self._fh.tell()
-        if size == 0:
-            return {}
-
-        self._fh.seek(0)
-        return json.load(self._fh)
-
-    def write(self, data):
-        self._fh.seek(0)
-        self._fh.write(json.dumps(data))
-        self._fh.flush()
-        os.fsync(self._fh.fileno())
-        self._fh.truncate()
-
-    def close(self):
-        self._fh.close()
-
-    @staticmethod
-    def touch(fname: pathlib.Path, create_dirs: bool) -> None:
-        if create_dirs:
-            base_dir = os.path.dirname(fname)
-            if not os.path.exists(base_dir):
-                os.makedirs(base_dir)
-
-        if not os.path.exists(fname):
-            with open(fname, 'a'):
-                os.utime(fname, None)
+    def get_external(self, native_id):
+        return self.data['native'][native_id]
 
 
-class Database:
-    def __init__(self,
-                 storage: typing.Type[Storage] = MemoryStorage,
-                 **kwargs: typing.Dict[str, typing.Any]):
-        self.storage = storage(**kwargs)
-        self.lock = threading.Lock()
-        self.tables: typing.Dict[str, typing.Type[Table]] = {}
-        self.data: DataContainer = self.storage.read() or {}
+class States(nodb.Table):
+    def save(self, src, state):
+        self.data[src.id] = state
 
-    def __del__(self) -> None:
-        self.storage.close()
-
-    def sync(self) -> None:
-        self.storage.write(self.data)
-
-    def create_table(self, name, cls):
-        if name not in self.data:
-            self.data[name] = {}
-        self.sync()
-
-        self.tables[name] = cls(self, self.data[name])
-        return self.tables[name]
-
-    def table(self, name):
-        return self.tables[name]
-
-    def __getattr__(self, attr):
-        tables = getattr(self, 'tables')
-        try:
-            return tables[attr]
-        except KeyError as e:
-            raise TableError(attr) from e
+    def get(self, src):
+        return self.data[src.id]
 
 
-class TableError(Exception):
-    pass
+class Links(nodb.Table):
+    def link(self, src, entity):
+        self.data[src.id] = entity
 
+    def unlink(self, src, entity):
+        del(self.data[src.id])
 
-DataContainer = typing.Dict[str, typing.Any]
-
-
-class Table:
-    def __init__(self, db: Storage, data: DataContainer):
-        self.db = db
-        self.data = data
-
-    @contextlib.contextmanager
-    def lock(self):
-        self.db.acquired()
-        yield self
-        self.db.release()
-
-    def sync(self):
-        self.db.sync()
-
-
-class KeyValueTable(Table):
-    def set(self, k, v):
-        self.data[k] = v
-        self.sync()
-
-    def get(self, k):
-        return self.data[k]
-
-    def get_all(self):
-        return list(self.data.keys())
-
-    def remove(self, k):
-        del(self.data[k])
-
-
-class DocumentTable(Table):
-    def _last_id(self):
-        if not self.data:
-            return 0
-
-        return max(self.data.keys())
-
-    def insert(self, doc):
-        id_ = self._last_id() + 1
-        self.data[id_] = doc
-
-    def _search(self, fn):
-        for (id_, doc) in self.data.items():
-            if fn(doc):
-                yield((id_, doc))
-
-    def search(self, fn):
-        return [doc for (_, doc) in self._search(fn)]
-
-    def get(self, fn):
-        g = self._search(fn)
-        ret = next(g)
-        try:
-            next(g)
-        except StopIteration:
-            return ret
-
-        raise MultipleResultsError()
-
-    def delete(self, fn):
-        res = self._search(fn)
-        for (id_, doc) in res:
-            del(self.data[id_])
-
-        return [doc for (id_, doc) in res]
-
-
-class QueryError(Exception):
-    pass
-
-
-class MultipleResultsError(QueryError):
-    pass
+    def get_source(self, entity):
+        return {v: k for (k, v) in self.items()}[entity]
---
+++ arroyo/kit/nodb.py
@@ -0,0 +1,260 @@
+import abc
+import contextlib
+import threading
+import typing
+import pathlib
+import json
+import os
+import unittest
+
+# import tinydb
+# import tinydb.storages
+
+DataContainer = typing.Dict[str, typing.Any]
+
+
+class Storage:
+    def __init__(self, **kwargs: typing.Dict[str, typing.Any]):
+        pass
+
+    @abc.abstractmethod
+    def read(self) -> typing.Dict[str, typing.Any]:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def write(self, data: DataContainer) -> None:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def close(self) -> None:
+        raise NotImplementedError()
+
+
+class Table:
+    def __init__(self,
+                 data: DataContainer,
+                 notify_fn: typing.Callable,
+                 lock: typing.Optional[threading.Lock] = None):
+        self.data = data
+        self.notify = notify_fn
+        self.lock = lock
+
+    @contextlib.contextmanager
+    def transaction(self):
+        self.lock.acquired()
+        yield self
+        self.lock.release()
+
+    def sync(self):
+        self.notify()
+
+
+class Database:
+    def __init__(self,
+                 storage: typing.Type[Storage] = Storage,
+                 **kwargs: typing.Dict[str, typing.Any]):
+        self.storage = storage(**kwargs)
+        self.lock = threading.Lock()
+        self.tables: typing.Dict[str, typing.Type[Table]] = {}
+        self.data: DataContainer = self.storage.read() or {}
+
+    def __del__(self) -> None:
+        self.close()
+
+    def sync(self) -> None:
+        self.storage.write(self.data)
+
+    def create_table(self, name, cls):
+        if name not in self.data:
+            self.data[name] = {}
+        self.sync()
+
+        self.tables[name] = cls(self.data[name], self.sync, self.lock)
+        return self.tables[name]
+
+    def table(self, name):
+        return self.tables[name]
+
+    def close(self):
+        self.storage.write(self.data)
+        self.storage.close()
+
+    # def __getattr__(self, attr):
+    #     tables = getattr(self, 'tables')
+    #     try:
+    #         return tables[attr]
+    #     except KeyError as e:
+    #         raise TableNotFoundError(attr) from e
+
+
+#
+# Useful exceptions
+#
+class TableNotFoundError(Exception):
+    pass
+
+
+class QueryError(Exception):
+    pass
+
+
+class NoResultError(QueryError):
+    pass
+
+
+class MultipleResultsError(QueryError):
+    pass
+
+
+#
+# Storage implementations
+#
+
+class MemoryStorage(Storage):
+    def __init__(self):
+        self.memory = {}
+
+    def read(self):
+        return self.memory
+
+    def write(self, data):
+        self.memory = data
+
+    def close(self):
+        pass
+
+
+class JSONStorage(Storage):
+    def __init__(self, path: pathlib.Path):
+        super().__init__()
+        self.touch(path, create_dirs=True)  # Create file if not exists
+        self._fh = open(path, 'r+', encoding='utf-8')
+
+    def read(self):
+        self._fh.seek(0, os.SEEK_END)
+        size = self._fh.tell()
+        if size == 0:
+            return {}
+
+        self._fh.seek(0)
+        return json.load(self._fh)
+
+    def write(self, data):
+        self._fh.seek(0)
+        self._fh.write(json.dumps(data))
+        self._fh.flush()
+        os.fsync(self._fh.fileno())
+        self._fh.truncate()
+
+    def close(self):
+        self._fh.close()
+
+    @staticmethod
+    def touch(fname: pathlib.Path, create_dirs: bool) -> None:
+        if create_dirs:
+            base_dir = os.path.dirname(fname)
+            if not os.path.exists(base_dir):
+                os.makedirs(base_dir)
+
+        if not os.path.exists(fname):
+            with open(fname, 'a'):
+                os.utime(fname, None)
+
+
+#
+# Table implementations
+#
+
+class KeyValueTable(Table):
+    def set(self, k, v):
+        self.data[k] = v
+        self.sync()
+
+    def get(self, k):
+        return self.data[k]
+
+    def get_all(self):
+        return list(self.data.keys())
+
+    def remove(self, k):
+        del(self.data[k])
+
+
+class DocumentTable(Table):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        keys = list(self.data.keys())
+        for k in keys:
+            self.data[int(k)] = self.data.pop(k)
+
+    def _last_id(self):
+        if not self.data:
+            return 0
+
+        return max(self.data.keys())
+
+    def insert(self, doc):
+        id_ = self._last_id() + 1
+        self.data[id_] = doc
+        self.sync()
+
+    def _search(self, fn):
+        for (id_, doc) in self.data.items():
+            if fn(doc):
+                yield((id_, doc))
+
+    def search(self, fn):
+        return [doc for (_, doc) in self._search(fn)]
+
+    def get(self, fn):
+        g = self._search(fn)
+        try:
+            id_, doc = next(g)
+        except StopIteration:
+            raise NoResultError()
+
+        try:
+            next(g)
+        except StopIteration:
+            return doc
+
+        raise MultipleResultsError()
+
+    def delete(self, fn):
+        res = self._search(fn)
+        for (id_, doc) in res:
+            del(self.data[id_])
+
+        return [doc for (id_, doc) in res]
+
+
+class TestingDatabase(Database):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.kv = self.create_table('kv', KeyValueTable)
+        self.docs = self.create_table('docs', DocumentTable)
+
+
+class NoDBTest(unittest.TestCase):
+
+    def test_all(self):
+        try:
+            os.unlink('/tmp/nodb-test.json')
+        except FileNotFoundError:
+            pass
+
+        db = TestingDatabase(storage=JSONStorage, path='/tmp/nodb-test.json')
+        db.kv.set('foo', 1)
+        db.kv.set('bar', 2)
+        db.docs.insert({'name': 'foo', 'id': 1})
+        db.docs.insert({'name': 'bar', 'id': 2})
+
+        db2 = TestingDatabase(storage=JSONStorage, path='/tmp/nodb-test.json')
+        self.assertEqual(db2.kv.get('foo'), 1)
+        self.assertEqual(db2.kv.get('bar'), 2)
+        self.assertEqual(db2.docs.get(lambda x: x.get('name') == 'foo'),
+                         {'name': 'foo', 'id': 1})
+
+
+if __name__ == '__main__':
+    unittest.main()
